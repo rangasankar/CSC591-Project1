60f9b491498d142c323746139d84299f60a6f38f
Ranga Sankar <47298258+rangasankar@users.noreply.github.com>
2019-02-03 10:22:18 -0500

    Update activation.py

--- activation.py
+++ activation.py
@@ -6,9 +6,13 @@ import numpy as np
 
 
 def sigmoid(z):
-    """The sigmoid function."""
-    return 0
+#"""The sigmoid function."""
+    sigm = 1. / (1. + np.exp(-z))
+    return sigm
 
 def sigmoid_prime(z):
-    """Derivative of the sigmoid function."""
-    return 0
+#    """Derivative of the sigmoid function."""
+    sigm = 1. / (1. + np.exp(-z))
+    sigm_prime = sigm * (1. - sigm)
+    return (sigm_prime)
+
